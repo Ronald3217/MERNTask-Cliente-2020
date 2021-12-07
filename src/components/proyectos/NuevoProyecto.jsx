5e26043472531b18e11d92ff835a8bf35b200212
Ronald3217 <megasnake305@gmail.com>
2021-12-07 15:07:24 -0600

    Update NuevoProyecto.jsx
    Mostrando el formulario de Nuevo Proyecto de forma condicional.

--- src/components/proyectos/NuevoProyecto.jsx
+++ src/components/proyectos/NuevoProyecto.jsx
@@ -1,6 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import proyectoContext from '../../context/proyectos/proyectoContext';
 
 const NuevoProyecto = () => {
+  // obtener el state del formulario
+  const proyectosContext = useContext(proyectoContext);
+  const { formulario } = proyectosContext;
+  // tate para proyecto
   const [proyecto, guardarProyecto] = useState({
     nombre: '',
   });
@@ -29,21 +34,23 @@ const NuevoProyecto = () => {
         Nuevo Proyecto
       </button>
 
-      <form className="formulario-nuevo-proyecto" onSubmit={onSubmitProyecto}>
-        <input
-          type="text"
-          className="input-text"
-          placeholder="Nombre Proyecto"
-          name="nombre"
-          value={nombre}
-          onChange={onChangeProyecto}
-        />
-        <input
-          type="submit"
-          className="btn btn-primario btn-block"
-          value="Agregar Proyecto"
-        />
-      </form>
+      {formulario ? (
+        <form className="formulario-nuevo-proyecto" onSubmit={onSubmitProyecto}>
+          <input
+            type="text"
+            className="input-text"
+            placeholder="Nombre Proyecto"
+            name="nombre"
+            value={nombre}
+            onChange={onChangeProyecto}
+          />
+          <input
+            type="submit"
+            className="btn btn-primario btn-block"
+            value="Agregar Proyecto"
+          />
+        </form>
+      ) : null}
     </>
   );
 };
